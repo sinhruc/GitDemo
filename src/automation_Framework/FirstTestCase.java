fe90ed95ec9ec9836c3fbc6526cee0005e87c554
ruchir <sinharuchir28@gmail.com>
2021-06-23 17:45:36 +0530

    this is commit done

--- src/automation_Framework/FirstTestCase.java
+++ src/automation_Framework/FirstTestCase.java
@@ -4,7 +4,8 @@ public class FirstTestCase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        System.out.println("Hello First Test Case");
+        System.out.println("Selenium");
 	}
 
 }
